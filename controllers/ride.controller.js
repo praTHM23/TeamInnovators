ab9b78025abcc6acc33d43970fa31ea708a2cc68
praTHM23 <cspcce2023@gmail.com>
2023-04-21 01:45:11 +0530

    get filtered routes for commuters

--- controllers/ride.controller.js
+++ controllers/ride.controller.js
@@ -2,8 +2,8 @@ const Ride = require('../models/ride.Model');
 const polyline = require('@mapbox/polyline');
 const turf = require('@turf/turf');
 const { createClient } = require('@google/maps');
-
-
+const geolib = require('geolib');
+const S2 = require('s2-geometry').S2;
 const googleMapsClient = createClient({
     key: 'AIzaSyA3VVKFZuS_LeyCLtLHJwsH9buwcCf69mE',
     Promise: Promise // Assuming you're using a version of Node.js that supports Promise
@@ -55,35 +55,35 @@ exports.getrides = async (req, res) => {
 }
 
 exports.getridesForCommuter = async (req, res, next) => {
-    // console.log("hello")
-    // const { source, destination } = req.body;
+    const { source, destination } = req.body;
 
-    // try {
-    //     // Use Google Maps Directions API to calculate the route and retrieve the polyline
-    //     const response = await googleMapsClient.directions({
-    //         origin: `${source.latitude},${source.longitude}`,
-    //         destination: `${destination.latitude},${destination.longitude}`,
-    //         mode: 'driving' // or 'bicycling', 'walking', etc.
-    //     }).asPromise();
+    // Get all rides that intersect the line between the source and destination
+    const rides = await Ride.find({
+        $or: [
+            {
+                source: {
+                    $geoIntersects: {
+                        $geometry: {
+                            type: 'LineString',
+                            coordinates: [source.coordinates, destination.coordinates]
+                        }
+                    }
+                }
+            },
+            {
+                destination: {
+                    $geoIntersects: {
+                        $geometry: {
+                            type: 'LineString',
+                            coordinates: [source.coordinates, destination.coordinates]
+                        }
+                    }
+                }
+            }
+        ]
+    }).populate('userId');
 
-    //     // Decode the polyline to get an array of coordinates
-    //     const polylineCoords = polyline.decode(response.json.routes[0].overview_polyline.points);
-
-    //     // Find all rides where the driver's coordinates fall between the polyline coordinates
-    //     const matchingRides = await Ride.find({
-    //         $and: polylineCoords.map(coord => ({
-    //             'driver.source.latitude': { $lte: coord[0] },
-    //             'driver.source.longitude': { $lte: coord[1] },
-    //             'driver.destination.latitude': { $gte: coord[0] },
-    //             'driver.destination.longitude': { $gte: coord[1] }
-    //         }))
-    //     });
-
-    //     res.json(matchingRides);
-    // } catch (error) {
-    //     console.error(error); // log the error for debugging purposes
-    //     res.status(500).json({ message: 'An error occurred while processing your request.' });
-    // }
+    res.json({ rides });
 }
 
 
